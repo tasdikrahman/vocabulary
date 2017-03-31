ed466de27486200f33fb71b28f9c0d995b2eabac
tenorz007 <chizzy.alaedu@aun.edu.ng>
2017-03-31 17:26:02 +0100

    Bumped release version

--- setup.py
+++ setup.py
@@ -19,20 +19,20 @@ finally:
 
 setup(
   name = 'Vocabulary',
-  version = '0.0.5',
+  version = '0.0.6',
   author = 'Tasdik Rahman',
-  author_email = 'tasdik95@gmail.com', 
+  author_email = 'tasdik95@gmail.com',
   description = "Module to get meaning, synonym, antonym, part_of_speech, usage_example, pronunciation and hyphenation for a given word",
   long_description=long_description,
-  url = 'https://github.com/prodicus/vocabulary', 
+  url = 'https://github.com/prodicus/vocabulary',
   license = 'MIT',
   install_requires = [
     "requests==2.13.0",
   ],
-  ### adding package data to it 
+  ### adding package data to it
   packages=find_packages(exclude=['contrib', 'docs']),
   ###
-  download_url = 'https://github.com/prodicus/vocabulary/tarball/0.0.5', 
+  download_url = 'https://github.com/prodicus/vocabulary/tarball/0.0.6',
   classifiers = [
       'Intended Audience :: Developers',
       'License :: OSI Approved :: MIT License',
