6b5cfa43fd354921c9302da414fc5d3597df94df
Tasdik Rahman <tasdik95@gmail.com>
2017-04-06 11:30:08 +0530

    [travis] Fixing import error in travis
    in python2, encoding is not taken as an argument to open()

--- setup.py
+++ setup.py
@@ -15,8 +15,12 @@ __version__ = VERSION
 here = path.abspath(path.dirname(__file__))
 
 # get the dependencies and installs
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
+if sys.version_info[:2] <= (2, 7):
+    with open(path.join(here, 'requirements.txt')) as f:
+        all_reqs = f.read().split('\n')
+else:
+    with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+        all_reqs = f.read().split('\n')
 
 install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
 dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' not in x]
